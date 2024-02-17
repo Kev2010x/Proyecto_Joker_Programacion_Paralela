bbc775355e3c91d5e3163f778a0e139706e71519
Kevin Pineda <pinedasilupukevin.20@gmail.com>
2024-02-17 13:34:38 -0500

    update

--- Version_Final_Por_ahora/v4.cpp
+++ Version_Final_Por_ahora/v4.cpp
@@ -134,7 +134,7 @@ void gaussJordanSecuencial(std::vector<float>& AB, int n) {
 }
 
 int main() {
-    const int n = 700;
+    const int n = 500;
     srand(static_cast<unsigned>(time(nullptr)));
 
     std::vector<float> d_AB, d_AB_secuencial;
@@ -150,10 +150,12 @@ int main() {
     auto endTimeSecuencial = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> sequential_time = endTimeSecuencial - startTimeSecuencial;
 
-    //int blocksize = std::thread::hardware_concurrency();
+   // int blocksize = std::thread::hardware_concurrency();
    // int blocksize = 
     int blocksize = std::min(static_cast<int>(std::thread::hardware_concurrency()), 12);
     int numBlocks = ceil(n / static_cast<float>(blocksize));
+
+    std::cout << "Número de hilos utilizados: " << blocksize << std::endl;
 
     auto startTime = std::chrono::high_resolution_clock::now();
     gaussJordanParallel(d_AB, n, numBlocks);
